a1e88747ad481ac50f402bdd12bb6e549589ba6f
Christopher Del Basso <ccdelbasso@gmail.com>
2021-03-22 17:32:05 -0500

    Done.

---
+++ lib/test.rb
@@ -0,0 +1,5 @@
+def display_rainbow(colors=phrase)
+    colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+
+    phrase = puts "The colors of the rainbow are: R: #{colors[0]}, O: #{colors[1]}, Y: #{colors[2]}, G: #{colors[3]}, B: #{colors[4]}, I: #{colors[5]}, V: #{colors[6]}"
+end
